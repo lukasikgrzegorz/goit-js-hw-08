c68ad2dcff3c345ebf5c6aec27bd2dc0a2bc3a67
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-19 08:42:55 +0200

    first ex. ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+for (let item of galleryItems) {
+    
+    let newItem = document.createElement("div");
+    newItem.classList.add("gallery__item");
+    let newLink = document.createElement("a");
+    newLink.href = item.original;
+    newLink.classList.add("gallery__link");
+
+    let newImage = document.createElement("img");
+    newImage.classList.add("gallery__image");
+    newImage.src = item.preview;
+    newImage.alt = item.description;
+    newImage.setAttribute("data-source", item.original);
+
+    newItem.append(newLink);
+    newLink.append(newImage); 
+    gallery.append(newItem); 
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: false,});
+		
+
+	
