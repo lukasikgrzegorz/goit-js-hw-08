7b26105abeefe8094dadc754da1ac3a47ca50e0f
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-20 00:31:23 +0200

    ex.3 json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,22 @@
 import Vimeo from '@vimeo/player';
 import _throttle from 'lodash.throttle';
 
-const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const KEY = 'videoplayer-current-time';
 
 const player = new Vimeo('vimeo-player');
 
 player.on('loaded', () => {
-  const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+  const currentTime = localStorage.getItem(KEY) || 0;
   player.setCurrentTime(currentTime);
 });
 
 player.on(
   'timeupdate',
   _throttle(({ seconds }) => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+    localStorage.setItem(KEY, seconds);
   }, 1000)
 );
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import _throttle from 'lodash.throttle';
+
+const KEY = 'feedback-form-state';
+
+const data = {};
+
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+email.addEventListener('input', _throttle((e) => {
+    
+    data.email = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(data)); 
+    console.log(localStorage.getItem(KEY));   
+
+}, 500));
+
+message.addEventListener('input', _throttle((e) => {
+    
+    data.message = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(data));  
+    console.log(localStorage.getItem(KEY)); 
+
+}, 500));
+
+
 
