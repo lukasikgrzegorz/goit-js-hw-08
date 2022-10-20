30f3a4d9f54d7a4447584165fbd6308eb7894210
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-20 08:29:59 +0200

    ex.3 reduce number of listener

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,26 +2,32 @@ import _throttle from 'lodash.throttle';
 
 const KEY = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
+
 const data = {};
 
-const email = document.querySelector('input');
-const message = document.querySelector('textarea');
 
-email.addEventListener('input', _throttle((e) => {
+// Check local storage
+
+if (localStorage.getItem(KEY)) { 
+    let dataLocal = JSON.parse(localStorage.getItem(KEY));
+    form.email.value = dataLocal.email;
+    form.message.value = dataLocal.message;
+};
+
+// Listeners
+
+form.addEventListener('input', _throttle((e) => {
     
-    data.email = e.target.value;
+    data[e.target.name] = e.target.value;
     localStorage.setItem(KEY, JSON.stringify(data)); 
     console.log(localStorage.getItem(KEY));   
 
 }, 500));
 
-message.addEventListener('input', _throttle((e) => {
-    
-    data.message = e.target.value;
-    localStorage.setItem(KEY, JSON.stringify(data));  
-    console.log(localStorage.getItem(KEY)); 
+//submit
 
-}, 500));
+
 
 
 
